769fb87595ab2794ed0f181e8dd9571af860d5c8
Martin Müller <damadden88@googlemail.com>
2020-07-02 18:38:50 +0200

    vpc low and high level constructs

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Repo for demonstrating low level and hight level CDK Constructs
 
 npm run build && cdk synth VpcStack > vpc.yaml
+
+npm run build && cdk synth CfnVpcStack > cfnvpc.yaml
--- index.ts
+++ index.ts
@@ -1,19 +1,25 @@
-import { Vpc } from '@aws-cdk/aws-ec2';
+import { Vpc, CfnVPC } from '@aws-cdk/aws-ec2';
 import { Stack, App} from '@aws-cdk/core';
 
 class VpcStack extends Stack {
   constructor(app: App, id: string) {
     super(app, id);
-    // high level construct
+
     new Vpc(this, 'vpc', {});
-    
-    // low level construct
-    // new CfnVPC(this, 'vpc', {
-    //   cidrBlock: '10.0.0.0/16'
-    // });
   }
 }
+class CfnVpcStack extends Stack {
+  constructor(app: App, id: string) {
+    super(app, id);
+    
+    new CfnVPC(this, 'vpc', {
+      cidrBlock: '10.0.0.0/16'
+    });
+  }
+}
+
 const app = new App();
 new VpcStack(app, 'VpcStack');
+new CfnVpcStack(app, 'CfnVpcStack');
 
 app.synth();
